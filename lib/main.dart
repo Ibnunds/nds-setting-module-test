517bf93839ca550b19190f93a7119b79ff22495d
ibnuputraard <ibnu.putra@nawadata.id>
2025-03-11 13:27:14 +0700

    revert

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,12 @@ import 'package:settings_module/pages/detail_sccreen.dart';
 import 'package:settings_module/pages/setting_screen.dart';
 
 class SettingModule extends StatelessWidget {
-  final Function(String) onRouteChanged;
-
-  const SettingModule({super.key, required this.onRouteChanged});
+  const SettingModule({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Navigator(
       key: Get.nestedKey(1),
-      observers: [
-        GetObserver((routing) {
-          if (routing?.current != null) {
-            onRouteChanged(routing!.current); // Kirim current route ke host
-          }
-        }),
-      ],
       onGenerateRoute: (settings) {
         switch (settings.name) {
           case '/detail':
