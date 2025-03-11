c448844f79374688572f7267c7ad0923450edcaa
ibnuputraard <ibnu.putra@nawadata.id>
2025-03-11 12:03:11 +0700

    added observer

--- lib/pages/setting_screen.dart
+++ lib/pages/setting_screen.dart
@@ -7,7 +7,6 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Setting"), leading: BackButton()),
       body: SafeArea(
         child: Center(
           child: Column(
