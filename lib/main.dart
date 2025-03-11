ebc286f0a80bb04640102233ecced9a11e5bd6f5
ibnuputraard <ibnu.putra@nawadata.id>
2025-03-11 11:14:12 +0700

    update to widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,20 @@ import 'package:get/get.dart';
 import 'package:settings_module/pages/detail_sccreen.dart';
 import 'package:settings_module/pages/setting_screen.dart';
 
-void main() => runApp(const SettingModule());
-
 class SettingModule extends StatelessWidget {
   const SettingModule({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Setting Module',
-      initialRoute: "/setting",
-      routes: {
-        "/setting": (context) => SettingScreen(),
-        "/detail": (context) => DetailScreen(),
+    return Navigator(
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case "/detail":
+            return GetPageRoute(page: () => DetailScreen());
+          default:
+            return GetPageRoute(page: () => SettingScreen());
+        }
       },
-      home: SettingScreen(),
     );
   }
 }
