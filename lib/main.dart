75f44ce418bea951957579f79513ca75bb786145
ibnuputraard <ibnu.putra@nawadata.id>
2025-03-11 14:01:32 +0700

    added popscope

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,33 @@ import 'package:settings_module/pages/detail_sccreen.dart';
 import 'package:settings_module/pages/setting_screen.dart';
 
 class SettingModule extends StatelessWidget {
-  final Function(String?) onRouteChange;
-  const SettingModule({super.key, required this.onRouteChange});
+  const SettingModule({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Navigator(
-      key: Get.nestedKey(1),
-      observers: [
-        GetObserver((routing) {
-          onRouteChange(routing?.current);
-        }),
-      ],
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case '/detail':
-            return GetPageRoute(page: () => DetailScreen());
-          default:
-            return GetPageRoute(page: () => SettingScreen());
+    return PopScope(
+      canPop: false, // Mencegah keluar langsung dari module
+      onPopInvokedWithResult: (didPop, result) {
+        if (!didPop) {
+          // Jika masih ada halaman dalam stack, pop dari navigator module
+          if (Get.nestedKey(1)?.currentState?.canPop() ?? false) {
+            Get.back(id: 1);
+          } else {
+            Get.back(); // Jika sudah di SettingScreen, kembali ke host
+          }
         }
       },
+      child: Navigator(
+        key: Get.nestedKey(1),
+        onGenerateRoute: (settings) {
+          switch (settings.name) {
+            case '/detail':
+              return GetPageRoute(page: () => DetailScreen());
+            default:
+              return GetPageRoute(page: () => SettingScreen());
+          }
+        },
+      ),
     );
   }
 }
