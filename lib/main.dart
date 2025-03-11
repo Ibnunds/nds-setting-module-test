0d1f1c26fb1abaee1efab0279e9c981564f4260a
ibnuputraard <ibnu.putra@nawadata.id>
2025-03-11 13:46:17 +0700

    added observer

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,18 @@ import 'package:settings_module/pages/detail_sccreen.dart';
 import 'package:settings_module/pages/setting_screen.dart';
 
 class SettingModule extends StatelessWidget {
-  const SettingModule({super.key});
+  final Function(String?) onRouteChange;
+  const SettingModule({super.key, required this.onRouteChange});
 
   @override
   Widget build(BuildContext context) {
     return Navigator(
       key: Get.nestedKey(1),
+      observers: [
+        GetObserver((routing) {
+          onRouteChange(routing?.current);
+        }),
+      ],
       onGenerateRoute: (settings) {
         switch (settings.name) {
           case '/detail':
