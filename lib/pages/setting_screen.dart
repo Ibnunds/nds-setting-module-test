de620b8289582eeaea47d7af118dd1ddaeb01ba1
ibnuputraard <ibnu.putra@nawadata.id>
2025-03-11 11:50:28 +0700

    add appbar

--- lib/pages/setting_screen.dart
+++ lib/pages/setting_screen.dart
@@ -7,6 +7,7 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("Setting")),
       body: SafeArea(
         child: Center(
           child: Column(
